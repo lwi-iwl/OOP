65f5cf8b3348bc5828a906256d2d6a60a374fed7
lwi-wil <nikstrach@gmail.com>
2021-03-05 13:38:14 +0300

    Кнопки файла

--- src/by/bsuir/oop/lab/panel/Panel.java
+++ src/by/bsuir/oop/lab/panel/Panel.java
@@ -9,9 +9,9 @@ public class Panel extends JFrame {
         super("TipaPaint");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         JMenuBar menu = new JMenuBar();
-        menu.add(Button_Shapes());
-
-
+        menu.add(File());
+        menu.add(Edit());
+        //menu.add(Button_Shapes());
         JFrame jf = new JFrame("For2D");
         Dimension sSize = Toolkit.getDefaultToolkit().getScreenSize();
         jf.setJMenuBar(menu);
@@ -21,12 +21,41 @@ public class Panel extends JFrame {
         jf.setResizable(false);
     }
 
-    private JMenu Button_Shapes()
+    private JMenu File()
+    {
+        JMenu file = new JMenu("Файл");
+        JMenuItem create = new JMenuItem("Создать");
+        JMenuItem save = new JMenuItem("Сохранить");
+        JMenuItem save_as = new JMenuItem("Сохранить как");
+        JMenuItem open = new JMenuItem("Открыть");
+        file.add(create);
+        file.add(save);
+        file.add(save_as);
+        file.add(open);
+
+        return file;
+    }
+
+    private JMenu Edit()
+    {
+        JMenu edit = new JMenu("Редактирование");
+        JMenuItem undo = new JMenuItem("Назад");
+        JMenuItem redo = new JMenuItem("Вперёд");
+        edit.add(undo);
+        edit.add(redo);
+        return edit;
+    }
+
+    /*private JMenu Button_Shapes()
     {
         JMenu button_shapes = new JMenu("Фигуры");
-        JMenuItem open = new JMenuItem("Прямоугольник");
-        button_shapes.add(open);
+        JMenuItem direct = new JMenuItem("Прямая");
+        JMenuItem rectangle = new JMenuItem("Прямоугольник");
+        JMenuItem oval = new JMenuItem("Овал");
+        button_shapes.add(direct);
+        button_shapes.add(rectangle);
+        button_shapes.add(oval);
         return button_shapes;
-    }
+    }*/
 
 }
