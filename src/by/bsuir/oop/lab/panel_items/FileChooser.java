f620a00a5b3d079aabda40c962fe7f71cf40c906
lwi-wil <nikstrach@gmail.com>
2021-04-25 11:34:51 +0300

    error correction

--- src/by/bsuir/oop/lab/panel_items/FileChooser.java
+++ src/by/bsuir/oop/lab/panel_items/FileChooser.java
@@ -19,18 +19,20 @@ public class FileChooser extends Component {
         int result = fileChooser.showSaveDialog(this);
         if (result == JFileChooser.APPROVE_OPTION) {
             selectedFile = fileChooser.getSelectedFile();
-        }
-        fileChooser.setVisible(true);
-        if (selectedFile != null) {
-            try {
-                FileOutputStream file = new FileOutputStream(selectedFile.getAbsolutePath() + ".json");
-                ObjectOutputStream object = new ObjectOutputStream(file);
-                object.writeObject(board.shapes);
-                object.flush();
-            } catch (Exception ex) {
-                System.out.println(ex.getMessage());
+            fileChooser.setVisible(true);
+            if (selectedFile != null) {
+                try {
+                    FileOutputStream file = new FileOutputStream(selectedFile.getAbsolutePath() + ".json");
+                    ObjectOutputStream object = new ObjectOutputStream(file);
+                    object.writeObject(board.shapes);
+                    object.flush();
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+                return selectedFile.getAbsolutePath();
             }
-            return selectedFile.getAbsolutePath();
+            else
+                return null;
         }
         else
             return null;
@@ -42,27 +44,29 @@ public class FileChooser extends Component {
         int result = fileChooser.showOpenDialog(this);
         if (result == JFileChooser.APPROVE_OPTION) {
             selectedFile = fileChooser.getSelectedFile();
-        }
-        fileChooser.setVisible(true);
-        if (selectedFile != null) {
-            board.shapes.clear();
-            board.redo.clear();
-            board.oldshapes.clear();
-            try {
-                FileInputStream file = new FileInputStream(selectedFile.getAbsoluteFile());
-                ObjectInputStream object = new ObjectInputStream(file);
-                board.shapes = (ArrayList<Shape>) object.readObject();
+            fileChooser.setVisible(true);
+            if (selectedFile != null) {
+                board.shapes.clear();
                 board.redo.clear();
-                board.repaint();
-
-                object.close();
-                file.close();
                 board.oldshapes.clear();
-                board.oldshapes.addAll(board.shapes);
-            } catch (Exception ex) {
-                System.out.println(ex.getMessage());
+                try {
+                    FileInputStream file = new FileInputStream(selectedFile.getAbsoluteFile());
+                    ObjectInputStream object = new ObjectInputStream(file);
+                    board.shapes = (ArrayList<Shape>) object.readObject();
+                    board.redo.clear();
+                    board.repaint();
+
+                    object.close();
+                    file.close();
+                    board.oldshapes.clear();
+                    board.oldshapes.addAll(board.shapes);
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+                return selectedFile.getAbsolutePath();
             }
-            return selectedFile.getAbsolutePath();
+            else
+                return null;
         }
         else
             return null;
